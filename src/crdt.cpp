4fc4b8c297d37409088e454e704036543d85e662
Devin Efe Karadag <114683781+Devin-Efe-Karadag@users.noreply.github.com>
2026-05-27 20:26:00 +0300

    add immutable CRDT element and operation IDs

---
+++ src/crdt.cpp
@@ -0,0 +1,13 @@
+#include "crdt.hpp"
+#include <algorithm>
+#include <limits>
+namespace ce {
+void validate(const Operation &o) {
+  if (!o.id.replica || !o.id.counter || !o.time || o.time == std::numeric_limits<uint64_t>::max() ||
+      o.id.counter == std::numeric_limits<uint64_t>::max() || o.ref == o.id ||
+      ((o.ref.replica == 0) != (o.ref.counter == 0)) ||
+      (!o.insert && (o.ref == Id{} || o.value != 0)) ||
+      (o.insert && o.value != '\n' && (o.value < 32 || o.value > 126)))
+    throw std::runtime_error("invalid operation");
+}
+Crdt::Crdt() { nodes.emplace(Id{}, Element{}); }
